2b3857026c8e48ef00c33a614311a337de58d9d9
Matthew Goldfine <mgoldfi1@binghamton.edu>
2018-09-06 22:07:11 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,5 +6,6 @@ function driversWithRevenueOver(drivers,string) {
 
 function driverNamesWithRevenueOver(drivers,string) {
   debugger;
-  return driversWithRevenueOver(drivers,string).map(function(x) {x.name})
+  const array = driversWithRevenueOver(drivers,string)
+  return array.map(function(y) {y.name})
 }
