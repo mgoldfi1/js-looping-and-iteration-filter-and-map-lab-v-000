63f0b66940e3bc10a79cc15f85a76896463522aa
Matthew Goldfine <mgoldfi1@binghamton.edu>
2018-09-06 22:19:00 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,8 +4,10 @@ function driversWithRevenueOver(drivers,string) {
   return drivers.filter(function(x){return x.revenue > string})
 }
 
-function driverNamesWithRevenueOver(drivers,string) {
-  debugger;
-  const array = driversWithRevenueOver(drivers,string)
-  return array.map(function(y) {y.name})
-}
+function driverNamesWithRevenueOver(drivers,string,cb) {
+
+  return driversWithRevenueOver(drivers,string).map(function (y) {return y.name})
+    
+  }
+
+
